091b084fb6b145f4bc3f0aecf9dbc39ad8bb9aa1
agoudbg <agoudbg@gmail.com>
2024-10-25 22:13:05 +0800

    chore: switch to node-canvas

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createCanvas, loadImage } from "@napi-rs/canvas";
+import { createCanvas, loadImage } from 'canvas';
 
 // icon: string/Buffer/Uint8Array
 export type IconInput = string | Buffer | Uint8Array;
